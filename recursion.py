f9458acf10ad6943bf4b8ec1c0b4dd1bad7e6023
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-05-19 14:11:43 +0900

    Create recursion.py

---
+++ recursion.py
@@ -0,0 +1,14 @@
+import math
+"""
+
+Recursion => Iterative Algorithm [Loop]
+Psuedocode:
+
+"""
+def getFactorial(n):
+    if -1 < n < 2: 
+        return 1
+    else: 
+        return n * getFactorial(n-1)
+
+print(getFactorial(10))
